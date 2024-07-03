cfb81a6ce4f19ec9b5ef50052e1cad9d8ea40e59
HughMorris01 <gfarrell2@Live.com>
2024-07-03 11:23:22 -0400

    Add Contact Form Functionality

---
+++ public/scripts/contact.js
@@ -0,0 +1,104 @@
+// Contact Form
+const contactForm = document.querySelector('.contact-form');
+const senderName = document.getElementById('name');
+const senderEmail = document.getElementById('email');
+const senderSubject = document.getElementById('subject');
+const senderMessage = document.getElementById('message');
+const inputs = document.querySelectorAll('.form-input');
+const submitButton = document.getElementById('submit-btn');
+let sentMessages = 0;
+let errorMessages = 0;
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (validate() === false) {
+    return;
+  }
+
+  let formData = {
+    name: senderName.value,
+    email: senderEmail.value,
+    subject: senderSubject.value,
+    message: senderMessage.value,
+  };
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/contact', true);
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function () {
+    if (xhr.responseText === 'success') {
+      if (sentMessages === 0) {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Thank you, I've received your message.</h1>";
+      } else if (sentMessages === 1) {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Thank you. I've received your message, again. &#128513;</h1>";
+      } else {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Three is the limit, please try again later &#128526;</h1>";
+        senderName.disabled = true;
+        senderEmail.disabled = true;
+        senderSubject.disabled = true;
+        senderMessage.disabled = true;
+        submitButton.disabled = true;
+        submitButton.classList.add('disable-btn');
+      }
+      senderName.value = '';
+      senderEmail.value = '';
+      senderSubject.value = '';
+      senderMessage.value = '';
+
+      sentMessages++;
+    } else {
+      if (errorMessages === 0) {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>Uh, oh. Something went wrong.......</h1>";
+        errorMessages++;
+      } else {
+        document.querySelector('.success').innerHTML =
+          "<h2 class='message-sent'>There must be an issue, please try again later.</h1>";
+      }
+      senderName.value = '';
+      senderEmail.value = '';
+      senderSubject.value = '';
+      senderMessage.value = '';
+    }
+  };
+  xhr.send(JSON.stringify(formData));
+});
+
+const validate = () => {
+  if (senderName.value === '') {
+    document.getElementById('name-validation').innerHTML =
+      'Please enter your name before submitting';
+    senderName.classList.add('validation-failed');
+    return false;
+  }
+  if (senderEmail.value === '') {
+    document.getElementById('email-validation').innerHTML =
+      'Please enter your email before submitting';
+    senderEmail.classList.add('validation-failed');
+    return false;
+  }
+  if (senderSubject.value === '') {
+    document.getElementById('subject-validation').innerHTML =
+      'Please enter a subject before submitting';
+    senderSubject.classList.add('validation-failed');
+    return false;
+  }
+  if (senderMessage.value === '') {
+    document.getElementById('message-validation').innerHTML =
+      'A message must be entered before submitting';
+    senderMessage.classList.add('validation-failed');
+    return false;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener('input', (e) => {
+    input.classList.remove('validation-failed');
+    input.nextElementSibling.innerHTML = '';
+  });
+});
+// End of Contact Form
--- server.js
+++ server.js
@@ -30,6 +30,74 @@ app.get('/contact', (req, res) => {
   res.sendFile(__dirname + '/public/views/contact.html');
 });
 
+app.post('/contact', (req, res) => {
+  //   console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.HI_EMAIL,
+      pass: process.env.HI_PASSWORD,
+    },
+  });
+
+  const mailOptions1 = {
+    from: process.env.HI_EMAIL,
+    to: process.env.GREGS_EMAIL,
+    subject: `Message from: ${req.body.name} <${req.body.email}> : ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  const mailOptions2 = {
+    from: process.env.ZOHO_USERNAME,
+    to: process.env.JASONS_EMAIL,
+    subject: `Message from: ${req.body.name} <${req.body.email}> : ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  const confirmationOptions = {
+    from: process.env.HI_EMAIL,
+    to: req.body.email,
+    subject: `Message Receipt Confirmation`,
+    text: "We've received the message that you sent through our website and will get back to you shortly.",
+  };
+
+  if (req.body.name !== undefined) {
+    transporter.sendMail(mailOptions1, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.send('error');
+      } else {
+        res.send('success');
+        console.log('successfully sent email');
+      }
+    });
+
+    // if (req.body.name !== undefined) {
+    // transporter.sendMail(mailOptions2, (err, info) => {
+    //   if (err) {
+    //     console.log(err);
+    //     res.send('error');
+    //   } else {
+    //     res.send('success');
+    //     console.log('successfully sent email');
+    //   }
+    // });
+
+    transporter.sendMail(confirmationOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+        res.send('error');
+      } else {
+        res.send('success');
+        console.log('successfully sent email');
+      }
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}.`);
 });
